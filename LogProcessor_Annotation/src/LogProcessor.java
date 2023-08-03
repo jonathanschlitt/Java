c4d9002e07f633f608a527909286a9502f4708c9
jonathanschlitt <development@jonathanschlitt.de>
2023-08-03 13:21:56 +0200

    first commit

---
+++ LogProcessor_Annotation/src/LogProcessor.java
@@ -0,0 +1,27 @@
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic.Kind;
+
+@Autonject
+@SupportedAnnotationTypes("Log")
+@SupportedSourceVersion(SourceVersion.RELEASE_11)
+public class LogProcessor extends AbstractProcessor {
+
+  @Override
+  public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+    annotations.forEach(annotation -> {
+      roundEnv.getElementsAnnotatedWith(annotation).forEach(element -> {
+        processingEnv.getMessager().printMessage(Kind.NOTE, "Log found at " + element);
+      });
+    });
+
+    return true;
+
+  }
+}
